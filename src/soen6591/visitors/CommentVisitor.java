01b71d9dbfcb11a8c50866670bdd9fd4eedd8b0e
aniket2106 <aniket.tailor7@gmail.com>
2023-04-30 17:19:58 -0400

    Improvised Comment visitor and fixed some bugs. Added Documentation

--- src/soen6591/visitors/CommentVisitor.java
+++ src/soen6591/visitors/CommentVisitor.java
@@ -13,50 +13,66 @@ import soen6591.handlers.AntiPatternDetectorHandler;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 
 public class CommentVisitor extends ASTVisitor  {
-    List<String> lineComments = new ArrayList<String>();
-    List<String> blockComments = new ArrayList<String>();
-    CompilationUnit compilationUnit;
-    private String[] source;
+    List<String> lc = new ArrayList<String>();
+    CompilationUnit cu;
+    private String[] temp;
+    List<String> bc = new ArrayList<String>();
 
     public CommentVisitor(CompilationUnit compilationUnit, String[] source) {
         super();
-        this.compilationUnit = compilationUnit;
-        this.source = source;
+        this.cu = compilationUnit;
+        this.temp = source;
     }
 
+    /**
+     * Visits a LineComment node in the Abstract Syntax Tree (AST) of a Java compilation unit and extracts its content.
+     * Adds the content to a list of line comments.
+     *
+     * @param node the LineComment object to visit
+     * @return true to continue visiting the node's children, false to stop
+     */
     @Override
     public boolean visit(LineComment node) {
-        int startLineNumber = compilationUnit.getLineNumber(node.getStartPosition()) - 1;
-        String lineComment = source[startLineNumber].trim();
+        int startLineNumber = cu.getLineNumber(node.getStartPosition()) - 1;
+        String lineComment = temp[startLineNumber].trim();
 
-        lineComments.add(lineComment);
+        lc.add(lineComment);
 
         return super.visit(node);
     }
 
+    /**
+     * Visits a BlockComment node in the Abstract Syntax Tree (AST) of a Java compilation unit and extracts its content.
+     * Adds the content to a list of block comments.
+     *
+     * @param node the BlockComment object to visit
+     * @return true to continue visiting the node's children, false to stop
+     */
     @Override
     public boolean visit(BlockComment node) {
-        int startLineNumber = compilationUnit.getLineNumber(node.getStartPosition()) - 1;
-        int endLineNumber = compilationUnit.getLineNumber(node.getStartPosition() + node.getLength()) - 1;
+        int startLineNumber = cu.getLineNumber(node.getStartPosition()) - 1;
+        int endLineNumber = cu.getLineNumber(node.getStartPosition() + node.getLength()) - 1;
         StringBuffer blockComment = new StringBuffer();
 
         for (int lineCount = startLineNumber ; lineCount<= endLineNumber; lineCount++) {
 
-            String blockCommentLine = source[lineCount].trim();
+            String blockCommentLine = temp[lineCount].trim();
             blockComment.append(blockCommentLine);
             if (lineCount != endLineNumber) {
                 blockComment.append("\n");
             }
         }
-        blockComments.add(blockComment.toString());
+        bc.add(blockComment.toString());
 
         return super.visit(node);
     }
 
+    // return the line comments
     public List<String> getLineComments() {
-        return lineComments;
+        return lc;
     }
 
+    // return the block comments.
     public List<String> getBlockComments() {
-        return blockComments;}
+        return bc;}
 }
